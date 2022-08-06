ae8860144eb449f6eff4b216b6fb5327c3dbc70a
Daffa Prasetya <kumaraprastya@gmail.com>
2022-08-06 05:40:55 +0000

    change zipname
    Signed-off-by: Daffa Prasetya <kumaraprastya@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -48,7 +48,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="NoName-X00T"
+ZIPNAME="NoName"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
